237e6bd9a51abbbac507c480eead1522cc832d06
Jeremy Moseley <jeremy@moseley.dev>
2024-03-02 12:53:30 -0800

    restart with godot3, player moves

--- Player.gd
+++ Player.gd
@@ -1,56 +1,38 @@
 extends Area2D
 
-signal hit
-
-@export var speed = 400 # How fast the player will move (pixels/sec).
+export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
+export var rotation_speed = 5
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	hide()
-
-var angle = 0
-@export var rotation_speed = 5 # rads/sec?
+	position = screen_size / 2
+	rotation = PI / 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2() # The player's movement vector.
+	var rotation_dir = 0
+	if Input.is_action_pressed("turn_left"):
+		rotation_dir -= 1
+	if Input.is_action_pressed("turn_right"):
+		rotation_dir += 1
+	rotation += rotation_dir * rotation_speed * delta
 
-	if Input.is_action_pressed("rotate_right"):
-		angle += rotation_speed * delta
-		rotation = angle
-	if Input.is_action_pressed("rotate_left"):
-		angle -= rotation_speed * delta
-		rotation = angle
+	var move_direction = 0
 	if Input.is_action_pressed("move_forward"):
-		# rotate by 90 degrees
-		velocity = Vector2(1, 0).rotated(angle + PI/2).normalized() * speed
+		move_direction -= 1
 	if Input.is_action_pressed("move_backward"):
-		velocity = Vector2(-1, 0).rotated(angle + PI/2).normalized() * speed
+		move_direction += 1
 
-	# Move the player.
-	velocity = velocity * delta
+	var velocity = Vector2(move_direction, 0).rotated(rotation).normalized() * speed * delta
 	position += velocity
-
-	# move the player to the other side of the screen if it goes off the screen, including the width of the player
-	if position.x > screen_size.x + 16:
-		position.x = -16
-	if position.x < -16:
-		position.x = screen_size.x + 16
-	if position.y > screen_size.y + 16:
-		position.y = -16
-	if position.y < -16:
-		position.y = screen_size.y + 16
-
-
-func _on_body_entered(body):
-	hide() # Player disappears after being hit.
-	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
+	
+	# if the player fully moves outside the screen, wrap around
+	if position.x > screen_size.x:
+		position.x = 0
+	elif position.x < 0:
+		position.x = screen_size.x
+	if position.y > screen_size.y:
+		position.y = 0
+	elif position.y < 0:
+		position.y = screen_size.y
