8e50e88cf77c38fc33ea9a53632bf86c31cd876b
Jeremy Moseley <jeremy@moseley.dev>
2024-03-26 21:45:22 -0700

    radial menu

--- AirStrike.gd
+++ AirStrike.gd
@@ -1,49 +1,17 @@
 extends Node2D
 
-export var recharge_time = 2.0
+export var recharge_time = 10.0
 export(PackedScene) var bullet_scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$ShootTimer.wait_time = recharge_time
-	$ShootTimer.start()
+	pass
 
-func _on_ShootTimer_timeout():
-	shoot()
+var last_shot_at = 0.0
 
-func shoot():
-	# look at each quarter of the screen, and shoot at the quarter with the most mobs in the area
-	# the top left corner is (0, 0), bottom right is (750, 1280)
-	var mobs = get_tree().get_nodes_in_group("mobs")
-	if mobs.size() == 0:
+func shoot(position):
+	if OS.get_ticks_msec() - last_shot_at < recharge_time * 1000:
 		return
-	var top_left = 0
-	var top_right = 0
-	var bottom_left = 0
-	var bottom_right = 0
-	for mob in mobs:
-		var mob_pos = mob.get_global_position()
-		if mob_pos.x < 375:
-			if mob_pos.y < 640:
-				top_left += 1
-			else:
-				bottom_left += 1
-		else:
-			if mob_pos.y < 640:
-				top_right += 1
-			else:
-				bottom_right += 1
-	var max_mobs = max(top_left, max(top_right, max(bottom_left, bottom_right)))
-	if max_mobs == top_left:
-		shoot_at(Vector2(188, 320))
-	elif max_mobs == top_right:
-		shoot_at(Vector2(463, 320))
-	elif max_mobs == bottom_left:
-		shoot_at(Vector2(188, 960))
-	else:
-		shoot_at(Vector2(463, 960))
-
-func shoot_at(position):
 	var bullet = bullet_scene.instance()
 	bullet.set_target(position)
 	bullet.look_at(position)
--- HUD.gd
+++ HUD.gd
@@ -2,12 +2,19 @@ extends CanvasLayer
 
 signal start_game
 
+const AIRSTRIKE_TEXTURE = preload("res://art/torpedo.png")
+
 func _ready():
 	$LifeBar.hide()
 	$ScoreLabel.hide()
 	pause_mode = Node.PAUSE_MODE_PROCESS
 	$GhostCover.hide()
 	$GhostPlayer.hide()
+	$PowerupMenu.hide()
+	$PowerupMenu.set_items([
+		{ 'texture': AIRSTRIKE_TEXTURE, 'title': 'Air Strike', 'id': 'air_strike'},
+	])
+	player_click_radius = 50
 
 func show_message(text):
 	$Message.text = text
@@ -44,30 +51,33 @@ func _on_MessageTimer_timeout():
 func _on_Player_on_health_changed(_damage, health):
 	$LifeBar/ProgressBar.value = health
 
-export var player_click_radius = 100
+export var player_click_radius = 50
 var player_dragging = false
 
 func _input(event):
 	# Mouse in viewport coordinates.
 	if event is InputEventScreenTouch or (event is InputEventMouseButton and event.button_index == BUTTON_LEFT):
-		if (event.position - get_node('/root/Main/Player').position).length() < player_click_radius:
-			if event.pressed && !player_dragging:
-				get_tree().paused = true
-				player_dragging = true
-				$GhostPlayer.show()
-				$GhostPlayer.position = event.position
-				$GhostCover.show()
-				$GhostPlayer.rotation = get_node('/root/Main/Player').rotation
-				$GhostPath.show()
-				$GhostPath.clear_points()
-				$GhostPath.add_point(get_node('/root/Main/Player').position)
+		if event.pressed:
+			if (event.position - get_node('/root/Main/Player').position).length() < player_click_radius:
+				if !player_dragging:
+					player_dragging = true
+					$GhostPlayer.show()
+					$GhostPlayer.position = event.position
+					$GhostPlayer.rotation = get_node('/root/Main/Player').rotation
+					$GhostPath.show()
+					$GhostPath.clear_points()
+					$GhostPath.add_point(get_node('/root/Main/Player').position)
+			else:
+				$PowerupMenu.open_menu(event.position)
+			$GhostCover.show()
+			get_tree().paused = true
 
-		if !event.pressed && player_dragging:
-			player_dragging = false
-			get_tree().paused = false
-			$GhostPlayer.hide()
+		else:
+			if player_dragging:
+				player_dragging = false
+				$GhostPlayer.hide()
 			$GhostCover.hide()
-			# $GhostPath.hide()
+			get_tree().paused = false
 
 	if player_dragging && (event is InputEventMouseMotion or event is InputEventScreenDrag):
 		$GhostPlayer.position = event.position
@@ -75,3 +85,16 @@ func _input(event):
 		$GhostPlayer.rotation = lerp_angle($GhostPlayer.rotation, event.speed.angle(), 0.5)
 		if $GhostPath.get_point_count() == 0 or ($GhostPath.get_point_position(0) - event.position).length() > 20:
 			$GhostPath.add_point(event.position, 0)
+
+
+func _on_PowerupMenu_item_selected(id:String, position:Vector2):
+	match (id):
+		'air_strike':
+			get_node('/root/Main/AirStrike').shoot(position)
+		_:
+			print("Unknown powerup selected: ", id)
+	$PowerupMenu.hide()
+
+
+func _on_PowerupMenu_cancelled():
+	$PowerupMenu.hide()
